045f5aec56b76c770a4dd372f7141890898d9b49
shishkovda <shishkovda@mail.ru>
2021-02-21 03:24:41 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 git clone https://github.com/shishkovda/grow-box-admin.git
 cd grow-box-admin
+git pull
 mvn clean install
 cd ..
 
 git clone https://github.com/shishkovda/grow-box-admin-db.git
+cd grow-box-admin-db
+git pull
+cd ..
 
 docker-compose build
 docker-compose pull
